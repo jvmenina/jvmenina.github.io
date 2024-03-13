fde281cf4c6ceeb2aa44dd39049bfdf7665bc48a
jmichaelmenina <45510041+jmichaelmenina@users.noreply.github.com>
2024-03-13 18:00:09 +0800

    Added repo link to footer

--- src/section_Footer.tsx
+++ src/section_Footer.tsx
@@ -13,7 +13,7 @@ export function FooterSection() {
             The seals representing the <strong>University of the Philippines</strong> and the <strong>Department of Science and Technology - Science Education Institute </strong> are owned and trademarked by the respective entities they represent; I claim no ownership of these seals.
           </p>
           <p>
-            The design of the website, its logo, and my personal photo are provided by myself. This website is not monetized by myself in any way.
+            The design of this website, its logo, and my personal photo are provided by myself and can be seen <a href="https://github.com/jmichaelmenina/web-jmichael" target="_blank" rel="noreferrer">here</a>. This website is not monetized by myself in any way.
           </p>
         </div>
       </div>
